60caf517f620544c0cb92da26063f799653ce54a
Andrew Morrison <morro141@gmail.com>
2022-10-18 23:04:35 -0400

    add in urql for graphql fetching

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
+  import { createClient, setContextClient } from '@urql/svelte'
+
   import "../app.css";
+
+  const client = createClient({
+    url: 'http://localhost:3000/graphql',
+  })
+
+  setContextClient(client)
 </script>
 
 <slot />
